2111368cd2ce675923761338476a68d11893e343
Murphy812 <murmail@gmail.com>
2019-10-24 23:14:36 +0300

    fresh start

---
+++ get.php
@@ -0,0 +1,8 @@
+﻿<?php
+  include_once("utils.inc");
+
+  $id = isset($_GET["id"])?$_GET["id"]:-1;
+  $res = getData("location", $id);
+//  header("Content-type: application/json; charset=utf-8");
+  echo "$res";
+?>
